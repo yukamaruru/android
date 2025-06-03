2f7e5f469610c771384b96d515abcd21ec432cc8
yukamaru <yuun.vv07@gmail.com>
2025-06-03 22:16:51 +0900

    androidプロジェクト作成

---
+++ src/android/Android.java
@@ -0,0 +1,19 @@
+package android;
+
+public class Android {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Smartphone android = new Smartphone();
+		android.play();
+		android.stop();
+		android.next();
+		android.back();
+		android.call();
+		android.mail();
+		android.photo();
+		android.internet();
+	}
+
+}
