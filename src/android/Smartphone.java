fec35b952c42cba917e051c66842cd580f92b3a1
yukamaru <yuun.vv07@gmail.com>
2025-06-03 22:25:50 +0900

    android版継承

---
+++ src/android/Smartphone.java
@@ -0,0 +1,17 @@
+package android;
+
+public class Smartphone extends Mp3player {
+	public void call() {
+		System.out.println("電話");
+	}
+	public void mail() {
+		System.out.println("メール");
+	}
+	public void photo() {
+		System.out.println("写真");
+	}
+	public void internet() {
+		System.out.println("インターネット");
+	}
+
+}
